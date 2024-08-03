0ce9424d7cb7e7961fc5da10cce12503fe9c222a
Samir Bogati <samir23bogati@gmail.com>
2024-08-03 15:57:28 +0545

    added deploy workflow

---
+++ src/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { BsFillBellFill, BsFillEnvelopeFill, BsPersonCircle, BsSearch, BsJustify } from 'react-icons/bs';
+
+function Header({ OpenSidebar }) {
+  return (
+    <header className='header'>
+      <div className='menu-icon'>
+        <BsJustify className='icon' onClick={OpenSidebar} />
+      </div>
+      <div className='header-left'>
+        <BsSearch className='icon' />
+      </div>
+      <div className='header-right'>
+        <BsFillBellFill className='icon' />
+        <BsFillEnvelopeFill className='icon' />
+        <BsPersonCircle className='icon' />
+      </div>
+    </header>
+  );
+}
+
+Header.propTypes = {
+  OpenSidebar: PropTypes.func.isRequired,
+};
+
+export default Header;
